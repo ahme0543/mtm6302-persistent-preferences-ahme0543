0f620ba511ac751473bbbc3951a0a05b899ffa94
ahme0543 <ahme0543@algonquinlive.com>
2023-04-04 15:33:54 -0400

    completes the assignment

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ const themeSelect = document.getElementById('themeSelect');
 const listStyleSelect = document.getElementById('listStyleSelect');
 
 // Set default values
-let theme = 'themeSelect';
+let theme = 'light';
 let listStyle = 'list-group';
 
 // Load saved preferences, if any
 if (localStorage.getItem('theme')) {
-  applyTheme(localStorage.getItem('theme'));
-  themeSelect.value = localStorage.getItem('theme');
+  theme = localStorage.getItem('theme');
+  applyTheme(theme);
+  themeSelect.value = theme;
 }
 
 if (localStorage.getItem('listStyle')) {
-  applyListStyle(localStorage.getItem('listStyle'));
-  listStyleSelect.value = localStorage.getItem('listStyle');
+  listStyle = localStorage.getItem('listStyle');
+  applyListStyle(listStyle);
+  listStyleSelect.value = listStyle;
 }
 
 // Add event listeners
@@ -31,26 +33,25 @@ listStyleSelect.addEventListener('change', () => {
   localStorage.setItem('listStyle', listStyle);
 });
 
-    // Additional content for each item
-    const additionalContent = [
-      ' - groccery shopping',
-      ' - making dinner',
-      ' - laundry',
-      ' - homework',
-      ' - cleaning',
-    ];
+// Additional content for each item
+const additionalContent = [
+  ' - grocery shopping',
+  ' - making dinner',
+  ' - laundry',
+  ' - homework',
+  ' - cleaning',
+];
 
-    // Add items to the list
-    for (let i = 0; i < additionalContent.length; i++) {
-      const li = document.createElement('li');
-      const span = document.createElement('span');
-      span.textContent = additionalContent[i];
-      li.classList.add('list-group-item');
-      li.textContent = `Item ${i+1}`;
-      li.appendChild(span);
-      myList.appendChild(li);
-    }
-
+// Add items to the list
+for (let i = 0; i < additionalContent.length; i++) {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  span.textContent = additionalContent[i];
+  li.classList.add('list-group-item');
+  li.textContent = `Item ${i+1}`;
+  li.appendChild(span);
+  myList.appendChild(li);
+}
 
 // Apply the selected theme
 function applyTheme(theme) {
@@ -61,7 +62,19 @@ function applyTheme(theme) {
 // Apply the selected list style
 function applyListStyle(listStyle) {
   myList.className = '';
-  myList.classList.add(`listStyle-${listStyle}`);
+  myList.classList.add(listStyle);
+  
+  // Modify list item styles
+  const listItems = myList.querySelectorAll('.list-group-item');
+  for (let i = 0; i < listItems.length; i++) {
+    const listItem = listItems[i];
+    listItem.classList.remove('list-group-item-action', 'list-group-flush');
+    if (listStyle === 'list-group-item-action') {
+      listItem.classList.add('list-group-item-action');
+    } else if (listStyle === 'list-group-flush') {
+      listItem.classList.add('list-group-flush');
+    }
+  }
 }
 
 
